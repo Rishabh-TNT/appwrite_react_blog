c172a49e8de36d7e98e3bde638804c1ed8794ac9
Rishabh TNT <rishabh221@gmail.com>
2026-04-26 11:10:38 +0530

    updated all post to show only current user posts

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,14 +1,19 @@
+import { Query } from "appwrite";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import service from "../appwrite/services";
 import PostCard from "../components/PostCard";
 
 function AllPosts() {
   const [posts, setPosts] = useState([]);
+  const userid = useSelector((state) =>
+    state.userData ? state.userData.$id : null,
+  );
 
   useEffect(() => {
     const getPosts = async () => {
       try {
-        const rows = await service.getPosts();
+        const rows = await service.getPosts([Query.equal("userid", userid)]);
         rows && setPosts(rows.rows);
         console.log("rows: ", rows);
       } catch (error) {
@@ -18,13 +23,15 @@ function AllPosts() {
       }
     };
     getPosts();
-  }, []);
+  }, [userid]);
 
   return (
     <div className="home">
-      {posts.map((post) => (
-        <PostCard {...post} key={post.$id} />
-      ))}
+      {posts.length !== 0 ? (
+        posts.map((post) => <PostCard {...post} key={post.$id} />)
+      ) : (
+        <h1>No post by the user</h1>
+      )}
     </div>
   );
 }
